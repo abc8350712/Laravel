30de431c8d3a215ee119854b667ab0855a52590f
P-V-3\Administrator <abc8350712@gmai..com>
2019-06-08 14:18:06 +0800

    [update] all celebrity

--- public/scripts/python_script/src.py
+++ public/scripts/python_script/src.py
@@ -30,8 +30,13 @@ def getItems(json_dir):
 [names, features] = getItems(json_dir)
 query_embedding = getEmbedding(args.query_dir)
 distance = face_recognition.face_distance(features, query_embedding)
+zip_item = list(zip(distance, names))
+sorted_item = sorted(zip_item, key=lambda x: x[0])
 ind = argmin(distance)
-print(names[ind])
+
+for i in range(20):
+	print(sorted_item[i][1])
+#print(sorted_item[0][1], sorted_item[1][1], sorted_item[2][1])
 
 
 
